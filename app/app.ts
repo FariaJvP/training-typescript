b851e0ed6ad606f6d9dd5c40a41ea3916bf1d939
Joao Vitor <jvfaria.paz@gmail.com>
2022-07-22 00:53:48 -0300

    adds the app module into index html file and adds the ts file to run the event

---
+++ app/app.ts
@@ -0,0 +1,11 @@
+import { NegotiationController } from "./controllers/negotiation-controller.js";
+
+const negotiationController = new NegotiationController();
+const requestform = document.querySelector(".form");
+
+
+requestform.addEventListener('submit', (event: Event) => {
+    event.preventDefault();
+    negotiationController.toNegotiation();
+    negotiationController.clearForm();
+})
