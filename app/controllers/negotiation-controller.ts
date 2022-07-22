4a01ea1f48b4c847ec60935fb3e53cab44b34b00
Joao Vitor <jvfaria.paz@gmail.com>
2022-07-22 00:51:49 -0300

    adds the negotiation controller

--- app/controllers/negotiation-controller.ts
+++ app/controllers/negotiation-controller.ts
@@ -1,18 +1,30 @@
-export class NegotiationController {
-    private date: HTMLInputElement;
-    private quantity: HTMLInputElement;
-    private value: HTMLInputElement;
+import { Negotiation } from "../models/negotiation.js";
 
+export class NegotiationController {
+    private inputDate: HTMLInputElement;
+    private inputQuantity: HTMLInputElement;
+    private inputValue: HTMLInputElement;
+    
     constructor(){
-        this.date = document.querySelector("#date")
-        this.quantity = document.querySelector("#quantity")
-        this.value = document.querySelector("#value")
+        this.inputDate = document.querySelector("#date")
+        this.inputQuantity = document.querySelector("#quantity")
+        this.inputValue = document.querySelector("#value")
+    }
+    
+    toNegotiation() {
+
+        const regex = /-/g;
+        const date = new Date(this.inputDate.value.replace(regex, ','))
+
+        const negotiation = new Negotiation(
+            date, 
+            parseInt(this.inputQuantity.value), 
+            parseFloat(this.inputValue.value)
+        );     
     }
 
     clearForm(): void {
-        this.date.value ='';
-        this.quantity.value ='';
-        this.value.value ='';
-        this.date.focus();
+        document.querySelector('form').reset()
+        this.inputDate.focus();
     }
 }
