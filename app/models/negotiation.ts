6a7bc4afe1a80ff7d70de5cf0290b90eb466037d
Joao Vitor <jvfaria.paz@gmail.com>
2022-07-22 00:51:19 -0300

    adds negotiation model with cherry-pick

---
+++ app/models/negotiation.ts
@@ -0,0 +1,15 @@
+export class Negotiation {
+    private _date: Date;
+    private _quantity: number;
+    private _value: number;
+
+    constructor(date: Date, quantity: number, value:number){
+        this._date = date;
+        this._quantity = quantity;
+        this._value = value;
+    }
+
+    get date() {
+        return this._date;
+    }
+}
