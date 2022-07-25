55ca52df6aa480a208bbccec1d75dde33070c5a4
Joao Vitor <jvfaria.paz@gmail.com>
2022-07-25 14:53:13 -0300

    updates the controller to receive a valid request from dto

--- app/controllers/negotiation-controller.ts
+++ app/controllers/negotiation-controller.ts
@@ -1,41 +1,34 @@
-import { Negotiation } from "../models/negotiation.js";
 import { Negotiations } from "../models/negotiations.js";
 import { MessageView } from "../views/message-view.js";
 import { NegotiationView } from "../views/negotiation-view.js";
+import { NegotiationDTO } from "./negotiationDTO.js";
 
 export class NegotiationController {
-    private inputDate: HTMLInputElement;
-    private inputQuantity: HTMLInputElement;
-    private inputValue: HTMLInputElement;
+    private negotiationDTO;
     private negotiations = new Negotiations();
     private negotiationsView = new NegotiationView("#negotiationsView");
     private messageView = new MessageView("#viewMessage");
     
-    constructor(){
-        this.inputDate = document.querySelector("#date")
-        this.inputQuantity = document.querySelector("#quantity")
-        this.inputValue = document.querySelector("#value")
+    constructor(negotiationDTO: NegotiationDTO){
+        this.negotiationDTO = negotiationDTO;
         this.negotiationsView.update(this.negotiations);
     }
-    
-    public toNegotiation() {
 
-        const regex = /-/g;
-        const date = new Date(this.inputDate.value.replace(regex, ','))
-
-        const negotiation = new Negotiation(
-            date, 
-            parseInt(this.inputQuantity.value), 
-            parseFloat(this.inputValue.value)
-        );    
-        this.negotiations.addNegotiation(negotiation);
-        this.negotiationsView.update(this.negotiations);
-        this.messageView.update("negotiation added")
-        this.clearForm();
+    public receiveFormRequest(){
+        if(!this.negotiationDTO.isValidRequest()){
+            this.messageView.update("Is not possible to add a negotiation whose date does not represent a working day")
+        } else {
+            const negotiation = this.negotiationDTO.convertToNegotiation();
+            this.negotiations.addNegotiation(negotiation);
+            this.negotiationsView.update(this.negotiations);
+            this.messageView.update("negotiation added");
+            this.clearForm();
+        }
     }
 
     private clearForm(): void {
-        document.querySelector('form').reset()
-        this.inputDate.focus();
+        const form  = document.querySelector('form') as HTMLFormElement;
+        form.reset();
+        this.negotiationDTO.getInputDate.focus();
     }
 }
