85b7b5d8f58e6ae4c632610011e2189f14f370ca
Joao Vitor <jvfaria.paz@gmail.com>
2022-07-26 09:11:11 -0300

    changes the implementation to lambda

--- app/src/decorators/executionTime.ts
+++ app/src/decorators/executionTime.ts
@@ -1,14 +1,12 @@
 export function ExecutionTime() {
     return function (target: any, propertyKey: string, propertyDescriptor: PropertyDescriptor){
-        const originalMethod = propertyDescriptor.value = function(...originalArguments: any[]){
+        propertyDescriptor.value = (...originalArguments: any[]) => {
             const executionBegin = performance.now();
-            const returnOriginalMethod = originalMethod.apply(this, originalArguments);
+            this.apply(this, originalArguments);
             const executionEnd = performance.now();
-            const result = (executionBegin - executionEnd)/1000;
+            const result = (executionBegin-executionEnd)/1000;
             console.log(`${propertyKey}, execution time: ${result} seconds`);
-            returnOriginalMethod;
         };
-    
         return propertyDescriptor;
     }
 }
