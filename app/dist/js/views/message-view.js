344330924c9e680561859398ee7c86ce009a5d75
Joao Vitor <jvfaria.paz@gmail.com>
2022-07-27 15:13:58 -0300

    removes ignored files

--- app/dist/js/views/message-view.js
+++
@@ -1,8 +0,0 @@
-import { View } from "./view.js";
-export class MessageView extends View {
-    template(info) {
-        return `
-            <p class="alert alert-info">${info}</p>
-        `;
-    }
-}
